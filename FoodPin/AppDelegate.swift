f15a9ef90e673a3c20b25b9bcc4b1c836b8fafdb
JT9 <caleb.tsosie@asu.edu>
2017-10-15 00:07:19 -0700

    Changed Font and Added Phone Number to Detail View
    Changed the font of the Titles and added Phone number to the class, arrays and the detailViewController

--- FoodPin/AppDelegate.swift
+++ FoodPin/AppDelegate.swift
@@ -20,9 +20,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UINavigationBar.appearance().barTintColor = UIColor(red: 216.0/255.0, green: 74.0/255.0, blue: 32.0/255.0, alpha: 1.0)
         UINavigationBar.appearance().tintColor = UIColor.white
         
-        if let barFont = UIFont(name: "Avenir-Light", size: 24.0) {
+        if let barFont = UIFont(name: "AvenirNextCondensed-DemiBold", size: 24.0) {
             UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName:UIColor.white, NSFontAttributeName:barFont]
         }
+        
+        //Changes status bar to light instead of dark
+        UIApplication.shared.statusBarStyle = .lightContent
         
         return true
     }
